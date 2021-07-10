e7274662c0095479ac4f18fd86b62927cc04885c
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-07-10 15:20:24 +0300

    Add Company and Plane classes.

---
+++ src/Lab1/domain/Company.java
@@ -0,0 +1,8 @@
+package Lab1.domain;
+
+public class Company {
+
+    private long id;
+    private String name;
+
+}
---
+++ src/Lab1/domain/Plane.java
@@ -0,0 +1,13 @@
+package Lab1.domain;
+
+public class Plane {
+
+    private long id;
+    private String manufacturer;
+    private String model;
+    private double capacity;
+    private double lifting;
+    private int rangeFlight;
+    private int fuelBank;
+
+}
