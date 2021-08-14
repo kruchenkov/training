fc0e84caacdcb06a9044f99dd600084c8a159753
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-08-14 15:39:39 +0300

    implemented the read method, separates the text into sentences.

--- src/Lab2/Book.java
+++ src/Lab2/Book.java
@@ -1,4 +1,27 @@
 package Lab2;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Book {
+
+    public static List<String> read(String fileName) throws IOException {
+        FileInputStream fis = new FileInputStream(fileName);
+        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+        List<String> sentences = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            String[] cells = line.split("[.!?]\\s*");
+            Collections.addAll(sentences, cells);
+        }
+        br.close();
+        return sentences;
+    }
 }
