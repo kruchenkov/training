711c4e5bb669c6cf78b03d3ad8f0d08b1f0a18bf
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-08-15 21:27:02 +0300

    Implemented method readSentence

--- src/Lab2/repository/BookRepositoryImpl.java
+++ src/Lab2/repository/BookRepositoryImpl.java
@@ -4,6 +4,36 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-public class BookRepositoryImpl {
+public class BookRepositoryImpl implements BookRepository{
+    FileInputStream fis = null;
+
+    public List<String> readSentence(String fileName) {
+
+        try {
+            fis = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+        List<String> sentences = new ArrayList<>();
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] cells = line.toLowerCase().split("[.!?]\\s*");
+                sentences.addAll(Arrays.asList(cells));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentences;
+    }
+
+
 
 }
