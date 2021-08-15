904e62d384e6355f5c182bb170b128c5d4e95456
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-08-15 21:19:43 +0300

    Add method mapWords

--- src/Lab2/repository/BookRepository.java
+++ src/Lab2/repository/BookRepository.java
@@ -1,10 +1,13 @@
 package Lab2.repository;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BookRepository {
 
     List<String> readSentence(String fileName);
 
     List<String> readWords(String fileName);
+
+    Map<String, Integer> mapWords(List<String> words, String inputLetter);
 }
