173ec6be675e8c649511042da73b9f2907052ce4
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-08-15 21:27:34 +0300

    Implemented method readWords

--- src/Lab2/repository/BookRepositoryImpl.java
+++ src/Lab2/repository/BookRepositoryImpl.java
@@ -34,6 +34,38 @@ public class BookRepositoryImpl implements BookRepository{
         return sentences;
     }
 
+    public List<String> readWords(String fileName) {
+        try {
+            fis = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+        List<String> words = new ArrayList<>();
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] cells = line.toLowerCase().split("[.!?]\\s*");
+                String word[][] = new String[cells.length][];
+                for (int i = 0; i < cells.length; ++i) {
+                    word[i] = cells[i].split("[\\p{Punct}\\s]+");
+                }
+                for (String[] s : word) {
+                    words.addAll(Arrays.asList(s));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+
 
 
 }
