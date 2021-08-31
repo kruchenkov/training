ea55905a1b32e1f621a21cfd9ee60ad8a75df3a7
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-08-31 22:42:06 +0300

    Class for parsing errors

---
+++ Lab3/src/xml/PlaneErrorHandler.java
@@ -0,0 +1,27 @@
+package xml;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+
+public class PlaneErrorHandler implements ErrorHandler {
+    private static Logger logger = LogManager.getLogger(System.getProperty("user.dir") + "/Lab4/planes.xsd");
+
+    public void warning(SAXParseException e) {
+        logger.warn(getLineColumnNumber(e) + "-" + e.getMessage());
+    }
+
+    public void error(SAXParseException e) {
+        logger.error(getLineColumnNumber(e) + " - " + e.getMessage());
+    }
+
+    public void fatalError(SAXParseException e) {
+        logger.fatal(getLineColumnNumber(e) + " - " + e.getMessage());
+    }
+
+    private String getLineColumnNumber(SAXParseException e) {
+
+        return e.getLineNumber() + " : " + e.getColumnNumber();
+    }
+}
